0e63b6f49b35dfd4c9e8b6eff7fabac027d26be5
Dimon <omni395@gmail.com>
2024-06-27 22:59:39 +0300

    .

--- app/controllers/admin/admin_users/registrations_controller.rb
+++ app/controllers/admin/admin_users/registrations_controller.rb
@@ -5,7 +5,7 @@ module Admin
     layout 'admin'
 
     # before_action :configure_sign_up_params, only: [:create]
-    # before_action :configure_account_update_params, only: [:update]
+    before_action :configure_account_update_params, only: [:update]
 
     # GET /resource/sign_up
     def new
@@ -49,9 +49,9 @@ module Admin
     # end
 
     # If you have extra params to permit, append them to the sanitizer.
-    # def configure_account_update_params
-    #   devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    # end
+    def configure_account_update_params
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :role])
+    end
 
     def update_resource(resource, params)
       resource.password = params[:password] if params[:password].present?
