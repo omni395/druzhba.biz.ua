13903fe75e60e45dc4f3cdfbbf2de8fc10467bb9
Dimon <omni395@gmail.com>
2024-07-10 17:40:28 +0300

    ю

--- app/mailers/message_notifier_mailer.rb
+++ app/mailers/message_notifier_mailer.rb
@@ -2,6 +2,6 @@ class MessageNotifierMailer < ApplicationMailer
   def new_message_email(landing_message, email)
     @landing_message = landing_message
     @email = email
-    mail(to: email, subject: landing_message.name)
+    mail(to: email, subject: "Нове повідомлення від " + landing_message.name)
   end
 end
