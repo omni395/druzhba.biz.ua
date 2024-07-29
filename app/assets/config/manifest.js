5e5364083ee2ea67a1e4a86212ab5a711cfbbb33
Dimon <omni395@gmail.com>
2024-07-29 20:16:42 +0300

    .

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,8 @@
 //= link_tree ../images
 //= link_tree ../videos
 //= link application.css
+//= link application.tailwind.css
 //= link admin.css
 //= link_tree ../builds
 //= link admin.js
 //= link application.js
-//= link tailwind.css
