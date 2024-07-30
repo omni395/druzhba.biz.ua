e34badc9af8788d1b51ea81cb16c33badb92aa27
Dimon <omni395@gmail.com>
2024-07-30 21:11:24 +0300

    .

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,24 +1,24 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://druzhba.biz.ua"
+SitemapGenerator::Sitemap.default_host = "https://druzhba.biz.ua/"
 SitemapGenerator::Sitemap.create_index = true
 
 SitemapGenerator::Sitemap.create do
   {uk: :ukrainian, ru: :russian}.each_pair do |locale, name|
     group(sitemaps_path: "sitemaps/#{locale}/", filename: name) do
-      add root_path, changefreq: 'daily'
-      add prices_path
-      add about_path
-      add contacts_path
-      add faq_path
-      add services_path
-      add articles_path
+      add root_path(locale: locale), changefreq: 'daily'
+      add prices_path(locale: locale)
+      add about_path(locale: locale)
+      add contacts_path(locale: locale)
+      add faq_path(locale: locale)
+      add services_path(locale: locale)
+      add articles_path(locale: locale)
 
       Article.find_each do |article|
-        add articles_path(article), changefreq: 'daily', lastmod: article.updated_at
+        add articles_path(article, locale: locale), changefreq: 'daily', lastmod: article.updated_at
       end
     
       Service.find_each do |service|
-        add services_path(service), changefreq: 'daily', lastmod: service.updated_at
+        add services_path(service, locale: locale), changefreq: 'daily', lastmod: service.updated_at
       end 
     end
   end
