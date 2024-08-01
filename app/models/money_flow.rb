7357543179ad189a384d92f5a7a7aeb2e553725d
Dimon <omni395@gmail.com>
2024-08-01 20:58:53 +0300

    .

--- app/models/money_flow.rb
+++ app/models/money_flow.rb
@@ -1,7 +1,17 @@
 class MoneyFlow < ApplicationRecord
   belongs_to :admin_user
-  has_many :money_flow_details
+  has_many :money_flow_details, dependent: :destroy
   
-  validates :order_id, uniqueness: { allow_nil: true }
   validates :order_id, allow_blank: true, numericality: { allow_nil: true }
+
+  
+  after_save :total_amount_sum
+
+  private
+
+  def total_amount_sum
+    total_amount = self.money_flow_details.sum(:amount)
+    self.update_column(:total_amount, total_amount)
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -8,18 +8,25 @@ class Order < ApplicationRecord
 
   after_create :money_flow_record
   after_update :money_flow_record
+  after_destroy :destroy_flow_record
 
   private
 
   def money_flow_record
-    m = MoneyFlow.new
-    m.order_id = self.id
-    m.admin_user_id = self.admin_user_id
-    m.title = "title"
-    m.description = "description"
-    m.total_amount = self.price
-    m.money_flow_details.build(amount: self.price, money_flow_category_id: 1)
-    m.save!
+    if self.paid?
+      m = MoneyFlow.new
+      m.order_id = self.id
+      m.admin_user_id = self.admin_user_id
+      m.title = "title"
+      m.description = "description"
+      m.total_amount = self.price
+      m.money_flow_details.build(amount: self.price, money_flow_category_id: 1)
+      m.save!
+    end
+  end
+
+  def destroy_flow_record
+    MoneyFlow.find_by(order_id: self.id).destroy
   end
 
 end
