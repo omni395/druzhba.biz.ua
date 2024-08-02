7bba5497baef32109aaad0689774ef1107b093eb
Dimon <omni395@gmail.com>
2024-08-02 09:54:48 +0300

    .

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,5 @@ Rails.application.routes.draw do
     # Can be used by load balancers and uptime monitors to verify that the app is live.
     get "up" => "rails/health#show", as: :rails_health_check
   end
-  get "/*path", to: redirect("/#{I18n.default_locale}/%{path}", status: 302)
   
 end
