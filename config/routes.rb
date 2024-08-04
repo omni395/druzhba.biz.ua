85346f6a42272771f94831fa0879c30246349ffc
Dimon <omni395@gmail.com>
2024-08-04 13:05:26 +0300

    .

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,8 @@ Rails.application.routes.draw do
 
     # Sitemap
     get "sitemap", to: "landing#sitemap"
+    # GoogoeADS
+    #get "ads", to: "landing#ads", defaults: { format: 'text' }
     
     # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
     # Can be used by load balancers and uptime monitors to verify that the app is live.
