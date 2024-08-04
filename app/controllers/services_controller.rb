3abab355f180066d2766f41631eaf13398e1529c
Dimon <omni395@gmail.com>
2024-08-04 17:57:36 +0300

    .

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,16 +1,24 @@
 class ServicesController < ApplicationController
-  before_action { @page_title = "Послуги швейної майстерні у Кривому Розі" }
+  before_action { @page_title = "Послуги швейної майстерні ☞ДРУЖБА☜ у Кривому Розі" }
   before_action { @page_description }
 
   def index
     @services = Service.order(id: :asc)
-    @page_description = "Послуги швейної майстерні ☞ДРУЖБА☜ у Кривому Розі. Якісний ремонт будь-якої складності та індивідуальний пощив одягу"
+    if I18n.locale == :uk
+      @page_description = "Послуги портной у швейної майстерні ☞ДРУЖБА☜ у Кривому Розі. Якісний ремонт будь-якої складності та індивідуальний пощив одягу"
+    else
+      @page_description = "Услуги портной в швейной мастерской ☞ДРУЖБА☜ в Кривом Роге. Качественный ремонт любой сложности и индивидуальный покров одежды"
+    end
   end
 
   def show
     @service = Service.friendly.find(params[:id])
     @page_title = @service.title
-    @page_description = "Послуга швейної майстерні ☞ДРУЖБА☜ - " + @service.subtitle
+    if I18n.locale == :uk
+      @page_description = "Послуга швейної майстерні ☞ДРУЖБА☜ - " + @service.subtitle
+    else
+      @page_description = "Услуга швейной мастерской ☞ДРУЖБА☜ -" + @service.subtitle
+    end
   end
 
 end
