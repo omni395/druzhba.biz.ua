25e228afe1bbd33ccc684896d4f508ef269d196a
Dimon <omni395@gmail.com>
2024-08-05 22:11:39 +0300

    .

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,14 +6,16 @@ class Order < ApplicationRecord
   
   has_many :order_details, class_name: "order_details", foreign_key: "reference_id"
 
-  after_create :money_flow_record
-  after_update :money_flow_record
-  after_destroy :destroy_flow_record
+  after_create :new_money_flow_record
+  after_update :update_money_flow_record
+  after_destroy :destroy_money_flow_record
+
+  scope :order_paid, -> {where(paid: inpaid)}
 
   private
 
-  def money_flow_record
-    if self.paid == 1
+  def new_money_flow_record
+    if self.paid == "inpaid"
       m = MoneyFlow.new
       m.order_id = self.id
       m.admin_user_id = self.admin_user_id
@@ -25,7 +27,23 @@ class Order < ApplicationRecord
     end
   end
 
-  def destroy_flow_record
+  def update_money_flow_record
+    if self.paid == "inpaid"
+      m = MoneyFlow.find_or_create_by(order_id: self.id)
+      m.order_id = self.id
+      m.admin_user_id = self.admin_user_id
+      m.title = "title"
+      m.description = "description"
+      m.total_amount = self.price
+      m.money_flow_details.update_all(amount: self.price) #не рабочая схема. обновляет все записи.
+      m.money_flow_details.update_all(money_flow_category_id: 1)
+      m.save!
+    else
+      MoneyFlow.find_by(order_id: self.id).destroy
+    end
+  end
+
+  def destroy_money_flow_record
     MoneyFlow.find_by(order_id: self.id).destroy
   end
 
