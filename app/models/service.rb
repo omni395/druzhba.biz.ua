0b4d7620565256bf312a7fc6df23ed988743395f
Dimon <omni395@gmail.com>
2024-08-09 17:17:55 +0300

    refactoring view landing services

--- app/models/service.rb
+++ app/models/service.rb
@@ -25,4 +25,5 @@ class Service < ApplicationRecord
 
   scope :repair, -> { where(svc: 0) } 
   scope :sewing, -> { where(svc: 1) }
+  scope :rand, -> { where(id: [1..Service.all.count].sample(6)) }
 end
