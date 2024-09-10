0a58db659e4ec22c834c55b0ccf3bf57925d9a7e
Dimon <omni395@gmail.com>
2024-09-10 12:27:18 +0300

    .

--- config/initializers/double_entry.rb
+++ config/initializers/double_entry.rb
@@ -6,9 +6,13 @@ DoubleEntry.configure do |config|
     accounts.define(identifier: :cash)
     accounts.define(identifier: :accounts_receivable, scope_identifier: ->(order_id) { order_id })
     accounts.define(identifier: :salary, scope_identifier: ->(user_id) { user_id })
+    accounts.define(identifier: :expense, scope_identifier: ->(expense_category_id) { expense_category_id })
   end
 
   config.define_transfers do |transfers|
+    transfers.define(from: :cash, to: :expense, code: :expense_payment)
+    transfers.define(from: :expense, to: :cash, code: :expense_refund)
+
     transfers.define(from: :customer, to: :cash, code: :order_payment)
     transfers.define(from: :customer, to: :accounts_receivable, code: :order_receivable)
     transfers.define(from: :accounts_receivable, to: :cash, code: :order_payment)
