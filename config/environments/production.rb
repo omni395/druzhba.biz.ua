cf5b221782cff0eba58fde93317ffdb4bba515bf
Dimon <omni395@gmail.com>
2024-09-20 17:03:12 +0300

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  config.port = 3000
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
