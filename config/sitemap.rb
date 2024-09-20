899637da20345edd05c922de4c5895b9827c7841
Dimon <omni395@gmail.com>
2024-09-20 09:40:09 +0300

    .

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -11,7 +11,7 @@ SitemapGenerator::Sitemap.create do
       I18n.locale = locale
       add root_path(locale:), changefreq: 'daily'
       add prices_path(locale:)
-      add about_path(locale:)
+      #add about_path(locale:)
       add contacts_path(locale:)
       add faq_path(locale:)
       add services_path(locale:)
@@ -28,29 +28,29 @@ SitemapGenerator::Sitemap.create do
   end
 end
 
-SitemapGenerator::Sitemap.default_host = 'https://druzhba.biz.ua/'
-SitemapGenerator::Sitemap.create_index = true
-SitemapGenerator::Sitemap.compress = true
-
-SitemapGenerator::Sitemap.create do
-  { uk: :ukrainian, ru: :russian }.each_pair do |locale, name|
-    group(sitemaps_path: "sitemaps/#{locale}/", filename: name) do
-      I18n.locale = locale
-      add root_path(locale:), changefreq: 'daily'
-      add prices_path(locale:)
-      add about_path(locale:)
-      add contacts_path(locale:)
-      add faq_path(locale:)
-      add services_path(locale:)
-      add articles_path(locale:)
-
-      Article.friendly.find_each do |article|
-        add article_path(article, locale:), changefreq: 'daily', lastmod: article.updated_at
-      end
-
-      Service.friendly.find_each do |service|
-        add service_path(service.slug, locale:), changefreq: 'daily', lastmod: service.updated_at
-      end
-    end
-  end
-end
+#SitemapGenerator::Sitemap.default_host = 'https://druzhba.biz.ua/'
+#SitemapGenerator::Sitemap.create_index = true
+#SitemapGenerator::Sitemap.compress = true
+#
+#SitemapGenerator::Sitemap.create do
+#  { uk: :ukrainian, ru: :russian }.each_pair do |locale, name|
+#    group(sitemaps_path: "sitemaps/#{locale}/", filename: name) do
+#      I18n.locale = locale
+#      add root_path(locale:), changefreq: 'daily'
+#      add prices_path(locale:)
+#      add about_path(locale:)
+#      add contacts_path(locale:)
+#      add faq_path(locale:)
+#      add services_path(locale:)
+#      add articles_path(locale:)
+#
+#      Article.friendly.find_each do |article|
+#        add article_path(article, locale:), changefreq: 'daily', lastmod: article.updated_at
+#      end
+#
+#      Service.friendly.find_each do |service|
+#        add service_path(service.slug, locale:), changefreq: 'daily', lastmod: service.updated_at
+#      end
+#    end
+#  end
+#end
