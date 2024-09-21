cc6a153d50ef972911143c76a80e8e8c6e9822ae
Dimon <omni395@gmail.com>
2024-09-21 18:27:25 +0300

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,18 @@ Rails.application.configure do
   config.assets.compress = true
   #config.assets.css_compressor = :sass
   config.assets.css_compressor = nil
+  config.assets.js_compressor = :esbuild
+ 
+  config.esbuild = {
+  plugins: [
+    {
+      name: 'treeshaker',
+      options: {
+        # Опции для плагина
+      }
+    }
+  ]
+}
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
