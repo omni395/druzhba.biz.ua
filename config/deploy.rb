a573c9d05e2495f7f3df995e62462eb8c495326d
Dimon <omni395@gmail.com>
2024-09-25 14:46:15 +0300

    .

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,98 +15,19 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 3
 
-# RBenv paths
-# set :default_env, {
-#    path: '/home/deploy/.rbenv/plugins/ruby-build/bin:/home/deploy/.rbenv/plugins:/home/deploy/.rbenv/shims:$PATH',
-#    rbenv_root: '/home/deploy/.rbenv/shims'
-# }
-# set :rbenv_roles, :all
-# set :rbenv_ruby, '1.1.1'
-# set :rbenv_ruby_dir, '/home/deploy/.rbenv/versions/3.3.1'
-# set :rbenv_custom_path, '/home/deploy/.rbenv'
-
-# Optionally, you can symlink your database.yml and/or secrets.yml file from the shared directory during deploy
-# This is useful if you don't want to use ENV variables
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
-
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
-
-
-
 #-----------#
 
-#set :unicorn_pid, '/home/deploy/druzhba/shared/tmp/pids/unicorn.pid'
-#set :unicorn_bind, '/home/deploy/druzhba/shared/tmp/sockets/unicorn.sock'
+set :unicorn_pid, '/home/deploy/druzhba/shared/tmp/pids/unicorn.pid'
+set :unicorn_bind, '/home/deploy/druzhba/shared/tmp/sockets/unicorn.sock'
 
-
-#namespace :deploy do
-#  before :compile_assets, :run_purgecss
-
-#  task :run_purgecss do
-#    run "cd #{release_path} && yarn build:purgecss"
-#  end
-#end
 
 namespace :deploy do
-  desc 'Deploy application'
-  task :default do
-    invoke 'deploy:prepare'
-    invoke 'deploy:remote'
-  end
+  before :compile_assets, :run_purgecss
 
-  desc 'Prepare deployment'
-  task :prepare do
-    invoke 'deploy:git'
-    invoke 'deploy:unicorn'
-  end
-
-  desc 'Deploy remote'
-  task :remote do
-    invoke 'deploy:nginx'
-    invoke 'deploy:unicorn'
-  end
-
-  desc 'Update Nginx configuration'
-  task :nginx do
-    run "sudo ln -sf #{latest_release}/public #{current_path}/public"
-    run "sudo ln -sf #{latest_release}/shared #{current_path}/shared"
-    run "sudo ln -sf #{latest_release}/config/nginx.conf #{current_path}/config/nginx.conf"
-  end
-
-  desc 'Update Unicorn configuration'
-  task :unicorn do
-    run "sudo ln -sf #{latest_release}/config/unicorn.rb #{current_path}/config/unicorn.rb"
+  task :run_purgecss do
+    run "cd #{release_path} && yarn build:purgecss"
   end
 end
