d705bad8ca164c48fae1b31538fa3d7b127b9b37
Dimon <omni395@gmail.com>
2024-09-26 12:02:45 +0300

    .

--- app/decorators/admin/article_decorator.rb
+++ app/decorators/admin/article_decorator.rb
@@ -1,17 +1,5 @@
-# frozen_string_literal: true
-
 module Admin
   module ArticleDecorator
-    def created_at_display
-      created_at ? I18n.l(created_at) : ''
-    end
-
-    def updated_at_display
-      updated_at ? I18n.l(updated_at) : ''
-    end
-
-    def published_display
-      '<i class="bi bi-check-circle-fill h3 text-warning"></i>'.html_safe if published?
-    end
+    
   end
 end
--- app/models/admin/article.rb
+++ app/models/admin/article.rb
@@ -1,18 +1,22 @@
-# frozen_string_literal: true
-
 module Admin
   class Article < ::Article
+    belongs_to :service, foreign_key: 'service_id'
+
+    delegate :title, to: :service, prefix: true, allow_nil: true
+
     has_one_attached :image
     attr_accessor :remove_image
-
     before_validation { self.image = nil if remove_image.to_s == '1' }
 
-    scope :id_eq, lambda { |v|
-      where(id: v) if v.present?
-    }
 
-    scope :published_eq, lambda { |v|
-      where(published: false) if v.present?
-    }
+    scope :service_id_eq, ->(v) do
+      where(service_id: v) if v.present?
+    end
+
+    scope :published_eq, ->(v) do
+      where(published: v) if v.present?
+    end
+
+
   end
 end
