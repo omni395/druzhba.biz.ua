e33a298e852ad7277a49cfdfc262f3582c141a5d
Dimon <omni395@gmail.com>
2024-09-26 12:21:40 +0300

    .

--- app/decorators/admin/article_decorator.rb
+++ app/decorators/admin/article_decorator.rb
@@ -1,5 +1,21 @@
+# frozen_string_literal: true
+
 module Admin
   module ArticleDecorator
-    
+    def created_at_display
+      created_at ? I18n.l(created_at) : ''
+    end
+
+    def updated_at_display
+      updated_at ? I18n.l(updated_at) : ''
+    end
+
+    def published_display
+      if published?
+        '<i class="bi bi-check-circle-fill h3 text-warning"></i>'.html_safe
+      else
+        '<i class="bi bi-check-circle-fill h3 text-muted"></i>'.html_safe
+      end
+    end
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -28,4 +28,8 @@ class Article < ApplicationRecord
   def created_at_field
     attributes['created_at'].strftime('%d-%m-%Y %H:%M')
   end
+
+  def updated_at_field
+    attributes['updated_at'].strftime('%d-%m-%Y %H:%M')
+  end
 end
