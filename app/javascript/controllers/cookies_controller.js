9d0c73ec27b2f6dc14bc81c441a325a63e95a9a7
Dimon <omni395@gmail.com>
2024-10-02 18:46:14 +0300

    .

--- app/javascript/controllers/cookies_controller.js
+++ app/javascript/controllers/cookies_controller.js
@@ -63,6 +63,11 @@ export default class extends Controller {
         'cookie_domain': 'druzhba.biz.ua',
         'cookie_expires': 28 * 24 * 60 * 60
       });
+      gtag('config', 'GT-MK9554BK', {
+        'cookie_prefix': 'gt-druzhba.biz.ua',
+        'cookie_domain': 'druzhba.biz.ua',
+        'cookie_expires': 28 * 24 * 60 * 60
+      });
       gtag('config', 'G-B1HN94WVHN', {
         'cookie_prefix': 'g-druzhba.biz.ua',
         'cookie_domain': 'druzhba.biz.ua',
@@ -76,7 +81,7 @@ export default class extends Controller {
   setConsent(type) {
     const script = document.createElement("script");
     script.id = 'consent' + type;
-    script.textContent = 'window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments)};gtag("config", "GTM-TJBHGWFJ");gtag("config", "G-B1HN94WVHN");gtag("consent", "' + type + '", {"ad_storage": "' + (type === 'default'? 'denied' : 'granted') + '","ad_user_data": "' + (type === 'default'? 'denied' : 'granted') + '","ad_personalization": "' + (type === 'default'? 'denied' : 'granted') + '","analytics_storage": "' + (type === 'default'? 'denied' : 'granted') + '"})';
+    script.textContent = 'window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments)};gtag("config", "GTM-TJBHGWFJ");gtag("config", "G-B1HN94WVHN";gtag("config", "GT-MK9554BK");gtag("consent", "' + type + '", {"ad_storage": "' + (type === 'default'? 'denied' : 'granted') + '","ad_user_data": "' + (type === 'default'? 'denied' : 'granted') + '","ad_personalization": "' + (type === 'default'? 'denied' : 'granted') + '","analytics_storage": "' + (type === 'default'? 'denied' : 'granted') + '"})';
     document.getElementsByTagName('head')[0].appendChild(script);
   }
 
