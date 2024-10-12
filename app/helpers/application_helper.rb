e8f85115a7768f88b420cdb5467d8a2753d3cd52
Dimon <omni395@gmail.com>
2024-10-12 11:13:16 +0300

    .

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,57 @@ module ApplicationHelper
 
   def alternate_locale_link
     alternate_locale = I18n.locale == :uk ? :ru : :uk
-    alternate_url = url_for(request.params.merge(locale: alternate_locale))
+    current_path = request.path
+    current_params = request.query_parameters
+
+    begin
+      # Получаем текущий маршрут
+      current_route = Rails.application.routes.recognize_path(current_path)
+      
+      if current_path == '/' || current_path == '/ru' || current_path == '/uk'
+        # Если это корневой маршрут
+        alternate_path = "/#{alternate_locale}"
+      elsif current_route[:controller] && current_route[:action] == 'show'
+        # Если это RESTful маршрут с параметром :id
+        model_name = current_route[:controller].classify
+        model_class = model_name.constantize
+        
+        if model_class.respond_to?(:friendly)
+          # Находим текущий объект
+          current_object = model_class.friendly.find(current_route[:id])
+          
+          # Генерируем альтернативный путь
+          I18n.with_locale(alternate_locale) do
+            alternate_slug = current_object.friendly_id
+            alternate_path = url_for(controller: current_route[:controller],
+                                     action: 'show',
+                                     id: alternate_slug,
+                                     only_path: true,
+                                     locale: alternate_locale)
+          end
+        else
+          # Если модель не использует friendly_id
+          alternate_path = current_path.sub("/#{I18n.locale}/", "/#{alternate_locale}/")
+        end
+      else
+        # Для других случаев
+        alternate_path = current_path.sub("/#{I18n.locale}/", "/#{alternate_locale}/")
+      end
+    rescue => e
+      # Если что-то пошло не так, просто заменим текущую локаль на альтернативную
+      alternate_path = current_path == '/' ? "/#{alternate_locale}" : current_path.sub("/#{I18n.locale}/", "/#{alternate_locale}/")
+    end
+
+    # Добавляем текущие параметры запроса к альтернативному пути
+    alternate_url = alternate_path.presence || "/"
+    alternate_url += "?#{current_params.to_query}" if current_params.any?
+
+    # Убеждаемся, что URL начинается с /
+    alternate_url = "/#{alternate_url}" unless alternate_url.start_with?('/')
+
+    # Убедимся, что URL не пустой и не дублирует текущий URL
+    return nil if alternate_url.blank? || alternate_url == current_path
+
     tag.link(href: "https://druzhba.biz.ua#{alternate_url}", 
              hreflang: alternate_locale, 
              rel: "alternate")
