2730becf01b6f1ee83b16672596634dae8453b68
Dimon <omni395@gmail.com>
2024-10-16 13:51:30 +0300

    .

--- app/javascript/controllers/landing/index_controller.js
+++ app/javascript/controllers/landing/index_controller.js
@@ -1,34 +1,53 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="landing--gallery"
+const postControllerIndexFilePath = "app/javascript/controllers/landing/index_controller.js"
+
 export default class extends Controller {
+  initialize() {
+  }
+
   connect() {
-    // Получаем все кнопки для открытия и закрытия модальных окон
-    this.toggleButtons = this.element.querySelectorAll('[data-modal-toggle]');
-    this.hideButtons = this.element.querySelectorAll('[data-modal-hide]');
-    
-    // Обработчик для открытия модального окна
-    this.toggleButtons.forEach(button => {
-      button.addEventListener('click', (event) => {
-        event.preventDefault(); // Предотвращаем стандартное поведение
-        const modalId = button.dataset.modalToggle;
-        const modal = document.getElementById(modalId);
-        if (modal) {
-          modal.classList.remove('hidden');
-        }
-      });
+    this.exFunction();
+  }
+
+  exFunction() {
+    // Плавная прокрутка
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 400);
+        return false;
     });
 
-    // Обработчик для закрытия модального окна
-    this.hideButtons.forEach(button => {
-      button.addEventListener('click', (event) => {
-        event.preventDefault(); // Предотвращаем стандартное поведение
-        const modalId = button.dataset.modalHide;
-        const modal = document.getElementById(modalId);
-        if (modal) {
-          modal.classList.add('hidden');
-        }
-      });
+    // Toggle the modal
+    const openContactFormButton = document.getElementById('openContactForm');
+    const closeContactFormButton = document.getElementById('closeContactForm');
+    const contactFormModal = document.getElementById('contactFormModal');
+    
+    openContactFormButton.addEventListener('click', () => {
+        contactFormModal.classList.remove('hidden');
     });
-  }
+
+    closeContactFormButton.addEventListener('click', () => {
+        contactFormModal.classList.add('hidden');
+    });
+
+    // Toggle nav menu
+    const toggleNavMenu = document.getElementById('toggleNavMenu');
+    const navMenu = document.getElementById('navMenu');
+    toggleNavMenu.addEventListener('click', () => {
+        navMenu.classList.toggle('hidden');
+    });
+
+    // Animation effects with aos.js
+    document.addEventListener('turbo:load', () => { AOS.init(
+      {
+        'offset': 300,
+        'duration': 800,
+        'easing': 'ease-in-sine'
+        //'data-aos-once': true
+      }
+    ) });
+  };
 }
