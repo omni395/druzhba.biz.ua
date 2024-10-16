369716f62d1807fb0de63fd936b2d887ec000ab6
Dimon <omni395@gmail.com>
2024-10-16 11:30:22 +0300

    .

--- app/javascript/controllers/turbo_controller.js
+++ app/javascript/controllers/turbo_controller.js
@@ -4,10 +4,12 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     this.disableTurboForLinks();
+    console.log('connected');
   }
   // Запретить turbo
   disableTurboForLinks() {
     const links = this.element.querySelectorAll('a');
+    console.log('connected');
     links.forEach(link => {
       link.setAttribute('data-turbo', 'false')
     });
