36c50f8f825c53844a9321a810f369f4883dc32f
Dimon <omni395@gmail.com>
2024-10-16 15:50:47 +0300

    .

--- app/javascript/controllers/landing/gallery_controller.js
+++ app/javascript/controllers/landing/gallery_controller.js
@@ -3,13 +3,13 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="landing--gallery"
 export default class extends Controller {
   connect() {
-    this.toggleButtons = this.element.querySelectorAll('[data-modal-toggle]');
-    this.hideButtons = this.element.querySelectorAll('[data-modal-hide]');
-    
+    this.toggleButtons = this.element.querySelectorAll('[data-modal-toggle]'); // Исправлено на data-modal_toggle
+    this.hideButtons = this.element.querySelectorAll('[data-modal-hide]'); // Исправлено на data-modal_hide
+
     this.toggleButtons.forEach(button => {
       button.addEventListener('click', (event) => {
-        event.preventDefault(); // Предотвращаем стандартное поведение
-        const modalId = button.dataset.modalToggle;
+        event.preventDefault();
+        const modalId = button.dataset.modalToggle; // исправлено по атрибуту
         const modal = document.getElementById(modalId);
         if (modal) {
           modal.classList.remove('hidden');
@@ -19,20 +19,10 @@ export default class extends Controller {
 
     this.hideButtons.forEach(button => {
       button.addEventListener('click', (event) => {
-        event.preventDefault(); // Предотвращаем стандартное поведение
-        const modalId = button.dataset.modalHide;
+        event.preventDefault();
+        const modalId = button.dataset.modalHide; // исправлено по атрибуту
         const modal = document.getElementById(modalId);
         if (modal) {
-          modal.classList.add('hidden');
-        }
-      });
-    });
-
-    // Обработчик клика вне модального окна
-    document.addEventListener('click', (event) => {
-      const modals = document.querySelectorAll('[id^="image-modal-"]');
-      modals.forEach(modal => {
-        if (!modal.classList.contains('hidden') && event.target === modal) {
           modal.classList.add('hidden');
         }
       });
