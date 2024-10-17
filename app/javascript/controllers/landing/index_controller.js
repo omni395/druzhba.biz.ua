80afdcd0ccf39a73355a8a0450137814effa5958
Dimon <omni395@gmail.com>
2024-10-17 11:53:12 +0300

    .

--- app/javascript/controllers/landing/index_controller.js
+++ app/javascript/controllers/landing/index_controller.js
@@ -2,8 +2,6 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-    this.loadFlowbiteStylesheet();
-    this.loadFlowbiteScript();
     this.loadAdsScript();
     this.exFunction();
 
