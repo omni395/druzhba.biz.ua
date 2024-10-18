92da7eb337f68d5f16c6ab9d2d480381cd7a4a9e
Dimon <omni395@gmail.com>
2024-10-18 12:27:27 +0300

    .

--- app/javascript/controllers/cookies_controller.js
+++ app/javascript/controllers/cookies_controller.js
@@ -7,6 +7,7 @@ export default class extends Controller {
     console.log('connected');
     if (this.areCookiesAllowed()) {
       this.appendGACode();
+      this.appendFacebookPixel();
       this.setConsent('update');
     } else {
       this.setConsent('default');
