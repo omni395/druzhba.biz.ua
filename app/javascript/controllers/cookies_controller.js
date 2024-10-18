78626adc4481a40cf013cf3fe15c407b68def072
Dimon <omni395@gmail.com>
2024-10-18 12:15:11 +0300

    .

--- app/javascript/controllers/cookies_controller.js
+++ app/javascript/controllers/cookies_controller.js
@@ -71,18 +71,26 @@ export default class extends Controller {
   
   // Append Facebook
   appendFacebookPixel() {
+    console.log("Facebook Pixel appending");
+    // Создаем новый элемент script
+    const script = document.createElement('script');
+    // Добавляем код Facebook Pixel в этот элемент
+    script.innerHTML = `
+        !function(f,b,e,v,n,t,s) {
+            if(f.fbq)return;
+            n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)
+        }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+        
+        fbq('init', '1562402937679169');
+        fbq('track', 'PageView');
+    `;
+    // Добавляем скрипт в head документа
+    document.head.appendChild(script);
     console.log("Facebook Pixel appended");
-    !function(f,b,e,v,n,t,s) {
-      if(f.fbq)return;
-      n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)
-    }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-    
-    fbq('init', '1562402937679169');
-    fbq('track', 'PageView');
   }
 
   setConsent(i) {
